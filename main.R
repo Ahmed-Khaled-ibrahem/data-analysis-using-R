6ef38da1071809e40563fefe89e082ba4ecaccdd
Ahmed khaled <93054790+Ahmed-Khaled-ibrahem@users.noreply.github.com>
2023-03-21 16:05:42 +0200

    first task

--- main.R
+++ main.R
@@ -1,5 +1,11 @@
-print("Basics")
+library(httr)
+library(rvest)
 
-rownames <- list("ahmed", "kaled","ziad")
-data <- data.frame(row.names = rownames)
-data
+get_wiki_covid19_page <- function() {
+  wiki_base_url <- "https://en.wikipedia.org/w/index.php"
+  url_parameters <- "?title=Template:COVID-19_testing_by_country"
+  response <- GET(paste0(wiki_base_url ,url_parameters))
+  return(response)
+}
+
+get_wiki_covid19_page()
