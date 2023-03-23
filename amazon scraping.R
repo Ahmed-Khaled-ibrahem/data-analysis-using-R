6fb04f4160624c5ac506395220ee94f47868ce14
Ahmed khaled <93054790+Ahmed-Khaled-ibrahem@users.noreply.github.com>
2023-03-23 19:45:28 +0200

    dataset exportation

--- amazon scraping.R
+++ amazon scraping.R
@@ -2,26 +2,24 @@ library(httr)
 library(rvest)
 
 baseurl <-"https://www.amazon.eg/s?i=electronics&rh=n%3A21832982031&fs=true&language=en&qid=1679591136&ref=sr_pg_2&page="
+df <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(df) <- c("title", "price", "image_link")
 
-# for(pageindex in 1:10){
-#   print(paste("progress is Page : ", pageindex))
-#   url <- paste0(baseurl,pageindex)
-#   document <- read_html(url)
-# }
-
-  url <- paste0(baseurl,1)
+for(pageindex in 1:10){
+  print(paste("progress is Page : ", pageindex))
+  url <- paste0(baseurl,pageindex)
   document <- read_html(url)
+
   allcards <- html_element(document,".s-search-results")
   cardshtml <- html_elements(allcards,".sg-col-4-of-24")
 
-     for (cardhtml in cardshtml){
-       image_link <- html_element(cardhtml,".s-image") %>% html_attr("src")
-       # image_link <- html_element(cardhtml,".s-image")
-       print(image_link)
+  for (cardhtml in cardshtml){
+    image_link <- html_element(cardhtml,".s-image") %>% html_attr("src")
+    title <- html_element(cardhtml,".a-size-mini") %>% html_text()
+    price <- html_element(cardhtml,".a-price-whole") %>% html_text()
 
-     }
-
-
-# print(html_text(cardhtml))
-
+    df[nrow(df) + 1,] <- c(title, price, image_link)
+  }
+}
+write.csv(df, "amazonTVs.csv")
 
